e9103566c134a10caee6f88458c2106557156c7f
Raffael Schmid <raffael@yux.ch>
2019-12-21 22:51:49 -0700

    Simplify Invoice List

--- src/InvoiceList.js
+++ src/InvoiceList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import useForm from 'react-hook-form';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { H1, Button, HTMLTable, MenuItem } from '@blueprintjs/core';
 import { Select } from '@blueprintjs/select';
@@ -11,16 +10,14 @@ import Amount from './Amount';
 function InvoiceList() {
   const [invoices, setInvoices] = useState(null);
   const [customers, setCustomers] = useState(null);
-  const { register, handleSubmit, setValue } = useForm();
-  const [selectedCustomer, setSelectedCustomer] = useState(null);
   const history = useHistory();
 
-  const onSubmit = data => {
-    const myData = {
-      ...data,
+  const createInvoice = customer => {
+    const data = {
+      customerId: customer.id,
       date: new Date(),
     };
-    send('create-invoice', myData).then((invoice) => {
+    send('create-invoice', data).then(invoice => {
       fetchInvoices();
       history.push(`/invoices/${invoice.id}/edit`);
     });
@@ -43,77 +40,56 @@ function InvoiceList() {
     fetchCustomers();
   }, []);
 
-  useEffect(() => {
-    register({ name: 'customerId' }, { required: 'benötigt' });
-  }, [register]);
-
   const showSkeleton = !invoices || !customers;
 
   return (
     <div>
       <H1>Rechnungen</H1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <HTMLTable>
-          <thead>
-            <tr>
-              <th>Nummer</th>
-              <th>Kunde</th>
-              <th>Total</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {(invoices || []).map(invoice => (
-              <tr key={invoice.id}>
-                <td>{invoice.number}</td>
-                <td>{invoice.customer.name}</td>
-                <td><Amount amount={invoice.total} /></td>
-                <td>
-                  <Link to={`/invoices/${invoice.id}/edit`}>
-                    <Button icon="edit" text="Bearbeiten" small={true} />
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="3">
-                <Select
-                  className={showSkeleton ? 'bp3-skeleton' : null}
-                  items={customers || []}
-                  itemRenderer={(customer, { handleClick }) => (
-                    <MenuItem
-                      key={customer.id}
-                      onClick={handleClick}
-                      text={`${customer.asTitle}`}
-                    />
-                  )}
-                  onItemSelect={customer => {
-                    setSelectedCustomer(customer);
-                    setValue('customerId', customer.id);
-                  }}
-                  noResults={<MenuItem disabled={true} text="No results." />}
-                >
-                  <Button
-                    text={selectedCustomer ? selectedCustomer.asTitle : 'Bitte wählen...'}
-                    rightIcon="double-caret-vertical"
-                  />
-                </Select>
+      <HTMLTable>
+        <thead>
+          <tr>
+            <th>Nummer</th>
+            <th>Kunde</th>
+            <th>Total</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {(invoices || []).map(invoice => (
+            <tr key={invoice.id}>
+              <td>{invoice.number}</td>
+              <td>{invoice.customer.name}</td>
+              <td>
+                <Amount amount={invoice.total} />
               </td>
               <td>
-                <Button
-                  className={showSkeleton ? 'bp3-skeleton' : null}
-                  type="submit"
-                  intent="primary"
-                  icon="add"
-                  text="Erstellen"
-                />
+                <Link to={`/invoices/${invoice.id}/edit`}>
+                  <Button icon="edit" text="Bearbeiten" small={true} />
+                </Link>
               </td>
             </tr>
-          </tfoot>
-        </HTMLTable>
-      </form>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="4">
+              <Select
+                className={showSkeleton ? 'bp3-skeleton' : null}
+                items={customers || []}
+                itemRenderer={(customer, { handleClick }) => (
+                  <MenuItem key={customer.id} onClick={handleClick} text={`${customer.asTitle}`} />
+                )}
+                onItemSelect={customer => {
+                  createInvoice(customer);
+                }}
+                noResults={<MenuItem disabled={true} text="No results." />}
+              >
+                <Button text="Neue Rechnung erstellen" rightIcon="double-caret-vertical" />
+              </Select>
+            </td>
+          </tr>
+        </tfoot>
+      </HTMLTable>
     </div>
   );
 }
